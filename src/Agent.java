da457a890dacbf308ace93a3e4800cda30939b4d
joacar <j.carselind@gmail.com>
2010-10-15 14:53:04 +0800

    Uploaded first files

--- src/Agent.java
+++ src/Agent.java
@@ -1,37 +1,87 @@
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.SoundSensor;
+
+
 /**
+ * The AI for the classic and legendary game Wumpus world!
+ * 
+ * The AI starts with percieving to see if anything new about
+ * the world can be noticed. If so, it takes that new
+ * information under considerations and via logic reasoning
+ * and a knowledge base decides what the most appropriate
+ * action to that percept and logic conclusion is. 
+ * 
+ * The sequence described above is looped over and over
+ * until it reaches it final state. That is the gold is
+ * collected and the robot is back to its starting location.
  * 
  * @author a0075840a and a0075885l
  *
  */
 public class Agent {
 	private WumpusWorld wumpusWorld;
-	private Location location;
+	private Position position;
+	private LightSensor light;
+	private SoundSensor sound;
 	
-	private static class Location {
-		Position prev = null;
-		Position cur = null;
-		
-		Location(Position cur) {
-			this.cur = cur;
-		}
-	}
-	
-	private static class Position {
+	/**
+	 * Just a simple holder for
+	 * x and y coordinates
+	 * 
+	 * @author a0075840a and a0075885l
+	 *
+	 */
+	public static class Position {
 		int x, y;
 		
 		Position(int x, int y) { 
 			this.x = x; this.y = y; 
 		}
 	}
+	public static void main(String args[]) {
+		new Agent();
+	}
 	
-	Agent() {
+	Agent() {		
+		light = new LightSensor(SensorPort.S3, true);
+		sound = new SoundSensor(SensorPort.S4, true);	// not sure with the port
 		
+		wumpusWorld = new WumpusWorld(light);
+	}
+	
+	void knowledgeBase() {
+		/*
+		 * A square [i,j] is adjacent to [x,y] if, (fA x,y,i,j) |x-i| or |y-j|
+		 * Formally: (fA i,j,x,y) Adjacent([i,j], [x,y]) <=> (|x-i| or |y-j|) 
+		 */
+		//boolean Adjacent(Position p1, Position p2) { return (Math.max(Math.abs(p1.x-p2.x), Math.abs(p1.y-p2.y)) == 1) ? true : false }
+		
+		/*
+		 * A square [x,y] is breezy if adjacent to a pit ( and [x,y] might
+		 * be a pit of some adjacent squares are breezy. )
+		 * Formally: (fA s) Breezy(s) (<=)=> (tE r) Adjacent(r,s) & Pit(r)
+		 */
+		// boolean Breezy(Position p) { State[] adj = getAdj(p); for(State s : adj) if(s.BREEZE & s.PIT ?) return true; return false; }
+		
+		/*
+		 * A square [x,y] is smelly if adjacent to wumpus ( and [x,y] might
+		 * contain the wumpus of some adjacent squares are smelly. )
+		 * Formally: (fA s) Smelly(s) (<=)=> (tE r) Adjacent(r,s) & Wumpus(r)
+		 */
+		// boolean Smelly(Position p) { State[] adj = getAdj(p); for(State s : adj) if(s.STENCH & s.WUMPUS ?) return true; return false; }
+		
+		/*
+		 * A pit is OK if there is neither pit nor wumpus in [x,y]
+		 * Formally: (fA p) not(Pit(p)) or not(Wumpus(p)) => Ok(p) 
+		 */
+		// boolean Ok(Position p) { return not(Pit(p)) || not(Wumpus(p)) }
 	}
 	
 	void welcomeMsg() throws InterruptedException {
 		System.out.println("What took you so long?!\n"+
-				"Been waiting forever for you to bring me back to life");
-		System.out.println("Initiliazing envoirnment settings");
+				"Been waiting forever for you to bring me back to life");		
+		System.out.println("Configuring system.");
 		Thread.sleep(10);
 		System.out.print(".");
 		Thread.sleep(10);
--- src/WumpusWorld.java
+++ src/WumpusWorld.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import lejos.nxt.Button;
+import lejos.nxt.LightSensor;
+
 /**
  * 
  * @author a0075840a and a0075885l
@@ -5,22 +13,95 @@
  */
 public class WumpusWorld {
 	final State GRID[][] = new State[7][7];
-	final int OK = 0, BREEZE = 1, STENCH, GLITTER, WUMPUS, PIT, STENCH_BREEZE, 
-		STENCH_GLITTER, GLITTER_BREEZE, STENCH_GLITTER_BREEZE, OUT_OF_RANGE;
+
 	
+	private HashMap<Agent.Position, State> exploredStates;
+	private ArrayList<HashSet<State>> adjacentList;
+	
+	final int BREEZE, STENCH, GLITTER, STENCH_BREEZE, OK, 
+	GLITTER_STENCH, GLITTER_BREEZE, STENCH_GLITTER_BREEZE;
+
 	private final static class State {
-		boolean OK = false, STENCH, GLITTER, BREEZE;
-		int WUMPUS = 0, PIT = 0;
+		boolean ok, stench, glitter, breeze;
+		boolean visited = false;
+		int wumpus = 0, pit = 0;
+		final boolean states[] = {ok, stench, glitter, breeze};
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("Current state is ");
+			for(int i = 8; i >= 1; i++) {
+				switch(i) {
+				case 1:
+					if(stench) sb.append("STENCH");
+				case 2:
+					if(glitter) sb.append("GLITTER");
+				case 3:
+					if(breeze) sb.append("BREEZE");
+				case 4:
+					if(stench && glitter) sb.append("STENCH + GLITTER");
+				case 5:
+					if(stench && breeze) sb.append("STENCH + BREEZE");
+				case 6:
+					if(breeze && glitter) sb.append("BREEZE + GLITTER");
+				case 7:
+					if(stench && glitter && breeze) sb.append("STENCH + BREEZE + GLITTER");
+				case 8:
+					if(ok) sb.append("OK");
+					return sb.toString();
+				}
+			}
+
+			return sb.toString();
+		}
 	}
-	
-	WumpusWorld() {
+
+	WumpusWorld(LightSensor ls) {
 		/*
 		 *  Begin with calibrating the light sensors for the different colours
-		 *  representing ok, breeze, smelly, etc
+		 *  representing ok, breeze, stench, etc
 		 */
-		int shadesValue[] = calibrateLigthSensor();
+		int tempLightValue = 0;
+
+		System.out.println("Place sensor above pure black...");
+		Button.waitForPress();
+		tempLightValue = ls.getLightValue();
+		OK = tempLightValue;
 		
+		System.out.println("Place sensor above pure white...");
+		Button.waitForPress();
+		ls.calibrateHigh();
+
+		tempLightValue = lightValue(ls, "GLITTER");
+		GLITTER = tempLightValue;
+		
+		tempLightValue = lightValue(ls, "BREEZE");
+		BREEZE = tempLightValue;
+
+		tempLightValue = lightValue(ls, "STENCH");
+		STENCH = tempLightValue;
+
+		tempLightValue = lightValue(ls, "STENCH + BREEZE");
+		STENCH_BREEZE = tempLightValue;
+
+		tempLightValue = lightValue(ls, "STENCH + GLITTER");
+		GLITTER_STENCH = tempLightValue;
+
+		tempLightValue = lightValue(ls, "GLITTER + BREEZE");
+		GLITTER_BREEZE = tempLightValue;
+
+		tempLightValue = lightValue(ls, "STENCH + GLITTER + BREEZE");
+		STENCH_GLITTER_BREEZE = tempLightValue;
+		
+		adjacentList = new ArrayList<HashSet<State>>();
 	}
-	
-	
+
+	private int lightValue(LightSensor ls, String text) {	
+		System.out.println("Place sensor above shade for "+text);
+		Button.waitForPress();
+		return ls.getLightValue();
+	}
+
+
 }
